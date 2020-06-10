37dba9a37be7764ccf9f50eb99559bf98534fc1b
Sam-aBii <samabii786@gmail.com>
2020-06-10 23:50:17 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, {useState} from 'react';
-import './App.css';
-import Test from "./test"
+import React, { useState } from "react";
+import "./App.css";
+import Test from "./test";
 
 function App() {
-  const myProfession = "MERN Stack Developer"
-  let { count,setCount } = useState(1);
+  const myProfession = "MERN Stack Developer";
+  let { count, setCount } = useState(1);
   return (
     <div className="App">
-     Hello From Rana Abid
-     Again checking for actions in job CI/CD 
-     <Test myName = "Rana Abid" myJob = {myProfession} />
-     <h2>Value of counter variable is: {count}</h2>
-     <button onClick = {
-       () => setCount(count + 1) }>Update Count
-      
-     </button>
+      Hello From Rana Abid Again checking for actions in job CI/CD
+      <Test myName="Rana Abid" myJob={myProfession} />
+      <h2>Value of counter variable is:{count}</h2>
+      <button onClick = { () => setCount(count + 1)}>Update Counter Number</button>
     </div>
   );
 }
