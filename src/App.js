0adda61602c74091932cc5990f424669270604d6
Sam-aBii <samabii786@gmail.com>
2020-06-11 00:48:07 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Test from "./test";
 
 function App() {
   const myProfession = "MERN Stack Developer";
-  let { count, setCount } = useState(1);
+  let [ count, setCount ] = useState(0);
   return (
     <div className="App">
       Hello From Rana Abid Again checking for actions in job CI/CD
       <Test myName="Rana Abid" myJob={myProfession} />
       <h2>Value of counter variable is:{count}</h2>
       <button onClick={() => setCount(count + 1)}>Update Counter Number</button>
+      <button onClick = {() => setCount(count - 1)}> decrement  </button>
     </div>
   );
 }
