6bbcaf2729be86ad59a05a5ccd1e134d2c0145ba
Sam-aBii <samabii786@gmail.com>
2020-06-17 23:53:44 +0500

    updated code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import{ Message} from "./Message"
 import Test from "./test";
 import { Parent } from "./Parent";
+import CounterContext from "./CounterContext";
 
 function App(props) {
   const myProfession = "MERN Stack Developer";
   let [count, setCount] = useState(1);
   return (
+    <CounterContext.Provider>
     <div className="App">
       Hello From Rana Abid Again checking for actions in job CI/CD
       <Test myName="Rana Abid" myJob={myProfession} />
@@ -19,6 +21,7 @@ function App(props) {
       <button onClick={() => setCount(--count)}> Decreament Button </button>
       <Parent name = "Rana Abid" />
     </div>
+    </CounterContext.Provider>
   );
 }
 
